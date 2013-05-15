0388127d409b823a20e772267f78988f797a055e
Victor Elias <victorgelias@gmail.com>
2013-05-15 00:54:47 -0300

    Adopting code conventions described also on the mocos.animation package.

--- src/com/marcioapf/mocos/animation/SpringInterpolator.java
+++ src/com/marcioapf/mocos/animation/SpringInterpolator.java
@@ -4,43 +4,43 @@ import android.view.animation.Interpolator;
 
 // TODO: Implement Runge-Kutta method instead of calling Math functions for performance.
 public class SpringInterpolator implements Interpolator {
-    private final double gamma, vDiv2;
-    private final boolean oscilative;
-    private final double eps;
+    private final double mGamma, mVDiv2;
+    private final boolean mOscilative;
+    private final double mEps;
 
-    private double A, B;
-    private double duration;
+    private double mA, mB;
+    private double mDuration;
 
     public SpringInterpolator(double tension, double damping) {
         this(tension, damping, 0.001);
     }
 
     public SpringInterpolator(double tension, double damping, double eps) {
-        this.eps = eps;
-        oscilative = (4 * tension - damping * damping > 0);
-        if (oscilative) {
-            gamma = Math.sqrt(4 * tension - damping * damping) / 2;
-            vDiv2 = damping / 2;
+        mEps = eps;
+        mOscilative = (4 * tension - damping * damping > 0);
+        if (mOscilative) {
+            mGamma = Math.sqrt(4 * tension - damping * damping) / 2;
+            mVDiv2 = damping / 2;
         } else {
-            gamma = Math.sqrt(damping * damping - 4 * tension) / 2;
-            vDiv2 = damping / 2;
+            mGamma = Math.sqrt(damping * damping - 4 * tension) / 2;
+            mVDiv2 = damping / 2;
         }
     }
 
     public void setInitialVelocity(double v0) {
-        if (oscilative) {
-            B = Math.atan(- gamma / (v0 - vDiv2));
-            A = -1 / Math.sin(B);
-            duration = Math.log(Math.abs(A) / eps) / vDiv2;
+        if (mOscilative) {
+            mB = Math.atan(-mGamma / (v0 - mVDiv2));
+            mA = -1 / Math.sin(mB);
+            mDuration = Math.log(Math.abs(mA) / mEps) / mVDiv2;
         } else {
-            A = (v0 - (gamma + vDiv2)) / (2 * gamma);
-            B = -1 - A;
-            duration = Math.log(Math.abs(A) / eps) / (vDiv2 - gamma);
+            mA = (v0 - (mGamma + mVDiv2)) / (2 * mGamma);
+            mB = -1 - mA;
+            mDuration = Math.log(Math.abs(mA) / mEps) / (mVDiv2 - mGamma);
         }
     }
 
     public double getDesiredDuration() {
-        return duration;
+        return mDuration;
     }
 
     @Override
@@ -48,9 +48,9 @@ public class SpringInterpolator implements Interpolator {
         if (input >= 1) {
             return 1;
         }
-        double t = input * duration;
-        return (float) (oscilative ?
-            (A * Math.exp(- vDiv2 * t) * Math.sin(gamma * t + B) + 1) :
-            (A * Math.exp((gamma - vDiv2) * t) + B * Math.exp(-(gamma + vDiv2) * t) + 1));
+        double t = input * mDuration;
+        return (float) (mOscilative ?
+            (mA * Math.exp(-mVDiv2 * t) * Math.sin(mGamma * t + mB) + 1) :
+            (mA * Math.exp((mGamma - mVDiv2) * t) + mB * Math.exp(-(mGamma + mVDiv2) * t) + 1));
     }
 }
