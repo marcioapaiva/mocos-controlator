e52b6d4259eca48bb28f868b503779fb1d115604
Victor Elias <victorgelias@gmail.com>
2013-05-15 01:07:51 -0300

    Applying code conventions to the classes on the mocos package.

--- src/com/marcioapf/mocos/AbsenceActivity.java
+++ src/com/marcioapf/mocos/AbsenceActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,112 +32,110 @@ import java.util.List;
 
 public class AbsenceActivity extends Activity {
 
-    ScrollView scrollView;
-	LinearLayout llMaterias;//, llPrincipal;
-	Button btnAdicionar, btnExportar;
-	ArrayList<LinMateria> arrLinMaterias;
-	LinMateria selected = null;
-	TextView tvFaltasTotais;
-    int totalAulasSemanais = 0;
-    int totalAtrasos = 0;
-	public static SharedPreferences sharedPrefTable;
-	SQLHelper sqlHelper;
-    
-	public static final int ACTIVITY_REQUEST_EDIT = 1;
-	public static final int ACTIVITY_REQUEST_IMPORT_EXPORT = 2;
-	public static final int ACTIVITY_RESULT_IMPORT_MADE = 2;
-	public static final String PREFS_NAME = "MyPrefsFile";
+    private ScrollView mScrollView;
+    private LinearLayout mLlMaterias;
+    private Button mBtnAdicionar;
+    private LinMateria mSelected = null;
+    private TextView mTvFaltasTotais;
+
+    private ArrayList<LinMateria> mLinMaterias;
+
+    private int mTotalAulasSemanais = 0;
+    private int mTotalAtrasos = 0;
+
+    private SQLHelper mSqlHelper;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
 
-    	sharedPrefTable = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-    	sqlHelper = new SQLHelper(this);
+    	mSqlHelper = new SQLHelper(this);
 
         setContentView(R.layout.main);
 
-        scrollView = (ScrollView) findViewById(R.id.scrllvwNo1);
-        llMaterias = (LinearLayout) findViewById(R.id.llmaterias);
-        tvFaltasTotais = (TextView) findViewById(R.id.tvFaltasTotais);
-        btnAdicionar = (Button) findViewById(R.id.btnNovaMateria);
+        mScrollView = (ScrollView) findViewById(R.id.scrllvwNo1);
+        mLlMaterias = (LinearLayout) findViewById(R.id.llmaterias);
+        mTvFaltasTotais = (TextView) findViewById(R.id.tvFaltasTotais);
+        mBtnAdicionar = (Button) findViewById(R.id.btnNovaMateria);
 
-		btnAdicionar.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				final LinMateria materia = new LinMateria(AbsenceActivity.this, "Nova", 4, false);
+		mBtnAdicionar.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                final LinMateria materia = new LinMateria(AbsenceActivity.this, "Nova", 4, false);
 
-                final ObjectAnimator btnAnimator = ObjectAnimator.ofFloat(btnAdicionar, "alpha", 0);
-                btnAnimator.setDuration(500);
-                btnAnimator.start();
+                final ObjectAnimator btAnimator = ObjectAnimator.ofFloat(mBtnAdicionar, "alpha", 0);
+                btAnimator.setDuration(500);
+                btAnimator.start();
                 createEditSubjectDialog(materia, new Runnable() {
-				    @Override
-				    public void run() {
-				        arrLinMaterias.add(materia);
-				        llMaterias.addView(materia);
-				        sqlHelper.insertAndID(materia.getData());
-
-                        ViewHelper.setTranslationX(materia, -llMaterias.getWidth());
-                        materia.animateToDelayed(0, 0, 700);
-
-                        btnAnimator.setFloatValues(1);
-                        btnAnimator.setStartDelay(1100);
-                        btnAnimator.start();
-				    }
-				}, new Runnable() {
                         @Override
                         public void run() {
-                            btnAnimator.setFloatValues(1);
-                            btnAnimator.start();
+                            mLinMaterias.add(materia);
+                            mLlMaterias.addView(materia);
+                            mSqlHelper.insertAndID(materia.getData());
+
+                            ViewHelper.setTranslationX(materia, -mLlMaterias.getWidth());
+                            materia.animateToDelayed(0, 0, 700);
+
+                            btAnimator.setFloatValues(1);
+                            btAnimator.setStartDelay(1100);
+                            btAnimator.start();
                         }
-                    }).show();
-				updateTotal();
-			}
-		});
+                    }, new Runnable() {
+                        @Override
+                        public void run() {
+                            btAnimator.setFloatValues(1);
+                            btAnimator.start();
+                        }
+                    }
+                ).show();
+                updateTotal();
+            }
+        });
 
 
-    	arrLinMaterias = new ArrayList<LinMateria>();
+    	mLinMaterias = new ArrayList<LinMateria>();
 
-    	ArrayList<MateriaData> materiasData = sqlHelper.retrieveAllMateriaData();
+    	ArrayList<MateriaData> materiasData = mSqlHelper.retrieveAllMateriaData();
     	for (MateriaData mData : materiasData){
-    		arrLinMaterias.add(new LinMateria(this, mData));
+    		mLinMaterias.add(new LinMateria(this, mData));
     	}
 
-        for (LinMateria lm : arrLinMaterias)
-	        llMaterias.addView(lm);
+        for (LinMateria lm : mLinMaterias)
+	        mLlMaterias.addView(lm);
 
         //Atualiza a contagem do total de faltas
         updateTotal();
-
     }
 
     public void updateTotal() {
-    	totalAulasSemanais = 0;
-    	totalAtrasos = 0;
+    	mTotalAulasSemanais = 0;
+    	mTotalAtrasos = 0;
 
-    	for (LinMateria materia : arrLinMaterias){
-    		totalAulasSemanais += materia.getAulasSemanais();
-    		totalAtrasos += materia.getAtrasos();
+    	for (LinMateria materia : mLinMaterias){
+    		mTotalAulasSemanais += materia.getAulasSemanais();
+    		mTotalAtrasos += materia.getAtrasos();
     	}
 
-    	if((int)(2*Math.ceil((float)0.10f*16*totalAulasSemanais)) - totalAtrasos <= 0.2f*(int)Math.ceil((float)0.10f*16*totalAulasSemanais)) {
-            if (tvFaltasTotais.getCurrentTextColor() != Color.RED){
-    		    AnimatorCreationUtil.ofTextColor(tvFaltasTotais, 300, Color.RED).start();
+    	if((int)(2*Math.ceil((float)0.10f*16* mTotalAulasSemanais)) - mTotalAtrasos <=
+                0.2f*(int)Math.ceil((float)0.10f*16* mTotalAulasSemanais)) {
+            if (mTvFaltasTotais.getCurrentTextColor() != Color.RED){
+    		    AnimatorCreationUtil.ofTextColor(mTvFaltasTotais, 300, Color.RED).start();
             }
     	}
-    	else if (tvFaltasTotais.getCurrentTextColor() != Color.DKGRAY) {
-            AnimatorCreationUtil.ofTextColor(tvFaltasTotais, 300, Color.DKGRAY).start();
+    	else if (mTvFaltasTotais.getCurrentTextColor() != Color.DKGRAY) {
+            AnimatorCreationUtil.ofTextColor(mTvFaltasTotais, 300, Color.DKGRAY).start();
     	}
 
-    	tvFaltasTotais.setText("Total: " + (float)totalAtrasos/2 + "/" + ((int)Math.ceil((float)0.10f*16*totalAulasSemanais)));
+    	mTvFaltasTotais.setText("Total: " + (float) mTotalAtrasos / 2 + "/" +
+            ((int) Math.ceil((float) 0.10f * 16 * mTotalAulasSemanais)));
     }
 
     protected void onPause() {
     	super.onPause();
     	//Retirar somente a data de cada matéria
     	ArrayList<MateriaData> materiasData = new ArrayList<MateriaData>();
-    	for (LinMateria lm : arrLinMaterias){
+    	for (LinMateria lm : mLinMaterias){
     		materiasData.add(lm.getData());
-    		sqlHelper.update(lm.getData());
+    		mSqlHelper.update(lm.getData());
     	}
     }
 
@@ -147,25 +144,25 @@ public class AbsenceActivity extends Activity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.context_menu, menu);
-        selected = (LinMateria) v;
+        mSelected = (LinMateria) v;
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        final LinMateria currentSelected = selected;
+        final LinMateria currentSelected = mSelected;
         switch (item.getItemId()) {
             case R.id.edit:
                 createEditSubjectDialog(currentSelected, new Runnable() {
                     @Override
                     public void run() {
-                        sqlHelper.update(currentSelected.getData());
+                        mSqlHelper.update(currentSelected.getData());
                     }
                 }).show();
                 return true;
             case R.id.remove:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Remover")
-                    .setMessage("Tem certeza que deseja remover \"" + selected.getStrNome() + "\"?")
+                    .setMessage("Tem certeza que deseja remover \"" + mSelected.getStrNome() +"\"?")
                     .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -176,12 +173,12 @@ public class AbsenceActivity extends Activity {
                 builder.show();
                 return true;
             case R.id.check:
-                selected.setCheckNeeded(true);
-                selected.update();
+                mSelected.setCheckNeeded(true);
+                mSelected.update();
                 return true;
             case R.id.notas:
                 Intent intent = new Intent(this, NotasActivity.class);
-                intent.putExtra("MateriaID", selected.getData().getSqlID());
+                intent.putExtra("MateriaID", mSelected.getData().getSqlID());
                 startActivity(intent);
                 return true;
             default:
@@ -196,7 +193,7 @@ public class AbsenceActivity extends Activity {
             public void run() {
                 AbsenceActivity.super.onBackPressed();
             }
-        }, animateAllTo(llMaterias.getWidth(), 0));
+        }, animateAllTo(mLlMaterias.getWidth(), 0));
     }
 
     /**
@@ -208,18 +205,19 @@ public class AbsenceActivity extends Activity {
      */
     private long animateAllTo(final float cardsTranslateX, float tvAndBtntAlpha) {
         final long betweenCardsDelay = 80;
-        int size = arrLinMaterias.size();
+        int size = mLinMaterias.size();
 
         for (int i = 0; i < size; i++)
-            arrLinMaterias.get(i).animateToDelayed(cardsTranslateX, 0, betweenCardsDelay * i);
+            mLinMaterias.get(i).animateToDelayed(cardsTranslateX, 0, betweenCardsDelay * i);
 
         long halfDuration = (betweenCardsDelay * size) / 2 + 120;
-        Animator anmtr = AnimatorCreationUtil.ofFloat(btnAdicionar, "alpha", halfDuration, null,
+        Animator anmtr = AnimatorCreationUtil.ofFloat(mBtnAdicionar, "alpha", halfDuration, null,
             tvAndBtntAlpha);
         anmtr.setStartDelay(halfDuration);
         anmtr.start();
 
-        anmtr = AnimatorCreationUtil.ofFloat(tvFaltasTotais, "alpha", halfDuration, null, tvAndBtntAlpha);
+        anmtr = AnimatorCreationUtil.ofFloat(mTvFaltasTotais, "alpha", halfDuration, null,
+            tvAndBtntAlpha);
         anmtr.setStartDelay(halfDuration);
         anmtr.start();
 
@@ -229,18 +227,18 @@ public class AbsenceActivity extends Activity {
     public void animateSubjectOut(final LinMateria materia) {
         final List<View> toBeAnimated = new ArrayList<View>();
         boolean after = false;
-        for (LinMateria mtr : arrLinMaterias) {
+        for (LinMateria mtr : mLinMaterias) {
             if (after)
                 toBeAnimated.add(mtr);
             if (mtr == materia)
                 after = true;
         }
-        toBeAnimated.add(btnAdicionar);
+        toBeAnimated.add(mBtnAdicionar);
         final int numberToBeAnimated = toBeAnimated.size();
 
-        int maxScroll = scrollView.getChildAt(0).getHeight() - scrollView.getHeight(),
+        int maxScroll = mScrollView.getChildAt(0).getHeight() - mScrollView.getHeight(),
             materiaHeight = materia.getHeight();
-        final int initScroll = scrollView.getScrollY(),
+        final int initScroll = mScrollView.getScrollY(),
                   scrollBy = ((maxScroll < initScroll + materiaHeight) ?
                       Math.max(maxScroll - materiaHeight, 0) : initScroll) - initScroll;
         ValueAnimator listAnimator = ValueAnimator.ofFloat(0, -materiaHeight);
@@ -251,26 +249,26 @@ public class AbsenceActivity extends Activity {
                 float value = (Float) animation.getAnimatedValue();
                 for (int i = 0; i < numberToBeAnimated; i++)
                     ViewHelper.setTranslationY(toBeAnimated.get(i), value);
-                ViewHelper.setScrollY(scrollView,
+                ViewHelper.setScrollY(mScrollView,
                     (int) (initScroll + scrollBy * animation.getAnimatedFraction()));
             }
         });
         listAnimator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                arrLinMaterias.remove(materia);
-                llMaterias.removeView(materia);
-                sqlHelper.remove(materia.getData().getSqlID());
+                mLinMaterias.remove(materia);
+                mLlMaterias.removeView(materia);
+                mSqlHelper.remove(materia.getData().getSqlID());
                 updateTotal();
-                scrollView.setVerticalScrollBarEnabled(true);
-                scrollView.setOnTouchListener(null);
+                mScrollView.setVerticalScrollBarEnabled(true);
+                mScrollView.setOnTouchListener(null);
                 for (View mtr : toBeAnimated)
                     ViewHelper.setTranslationY(mtr, 0);
             }
         });
         listAnimator.setDuration(700);
-        scrollView.setVerticalScrollBarEnabled(false);
-        scrollView.setOnTouchListener(new View.OnTouchListener() {
+        mScrollView.setVerticalScrollBarEnabled(false);
+        mScrollView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 return true;
@@ -278,7 +276,7 @@ public class AbsenceActivity extends Activity {
         }); //disable user scrolling during the animation
 
         if (ViewHelper.getTranslationX(materia) == 0)
-            materia.animateTo(llMaterias.getWidth(), 0);
+            materia.animateTo(mLlMaterias.getWidth(), 0);
         listAnimator.setStartDelay(500);
         listAnimator.start();
     }
@@ -341,6 +339,3 @@ public class AbsenceActivity extends Activity {
         return dialog;
     }
 }
-
-
-
--- src/com/marcioapf/mocos/NotasActivity.java
+++ src/com/marcioapf/mocos/NotasActivity.java
@@ -9,46 +9,46 @@ import com.marcioapf.mocos.data.MateriaMemos;
 import com.marcioapf.mocos.data.SQLHelper;
 
 public class NotasActivity extends Activity {
-	
-	long materiaID = 10;
-	EditText et1bim, et2bim, etExame;
-	TextView tvNomeMateria;
-	MateriaMemos mMemos;
-	SQLHelper sqlHelper;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_notas);
-		
-		
-		Intent intent = this.getIntent();
-		Bundle bundle = intent.getExtras();
-		materiaID = bundle.getLong("MateriaID", -1);
-		
-		sqlHelper = new SQLHelper(this);
-		
-		tvNomeMateria = (TextView)findViewById(R.id.tvNomeMateria);
-		et1bim = (EditText)findViewById(R.id.et1bim);
-		et2bim = (EditText)findViewById(R.id.et2bim);
-		etExame = (EditText)findViewById(R.id.etExame);
-		
-		mMemos = sqlHelper.retrieveMateriaMemosByID(materiaID);
-		
-		String strNomeMateria = sqlHelper.retrieveMateriaDataById(materiaID).getStrNome();
-		tvNomeMateria.setText(strNomeMateria + " - Notas");
-		et1bim.setText(mMemos.getS1bim());
-		et2bim.setText(mMemos.getS2bim());
-		etExame.setText(mMemos.getSExame());
-	}
-	
-	@Override
-	protected void onPause(){
-		super.onPause();
 
-		mMemos.setAll(et1bim.getText().toString(), et2bim.getText().toString(),
-							etExame.getText().toString());
-		sqlHelper.updateMemos(mMemos);
-	}
+    private long mMateriaID = 10;
+    private EditText mEt1bim;
+    private EditText mEt2bim;
+    private EditText mEtExame;
+    private TextView mTvNomeMateria;
+    private MateriaMemos mMemos;
+    private SQLHelper mSqlHelper;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+
+        Intent intent = this.getIntent();
+        Bundle bundle = intent.getExtras();
+        mMateriaID = bundle.getLong("MateriaID", -1);
+
+        mSqlHelper = new SQLHelper(this);
+
+        mTvNomeMateria = (TextView)findViewById(R.id.tvNomeMateria);
+        mEt1bim = (EditText)findViewById(R.id.et1bim);
+        mEt2bim = (EditText)findViewById(R.id.et2bim);
+        mEtExame = (EditText)findViewById(R.id.etExame);
+
+        mMemos = mSqlHelper.retrieveMateriaMemosByID(mMateriaID);
+
+        String strNomeMateria = mSqlHelper.retrieveMateriaDataById(mMateriaID).getStrNome();
+        mTvNomeMateria.setText(strNomeMateria + " - Notas");
+        mEt1bim.setText(mMemos.getS1bim());
+        mEt2bim.setText(mMemos.getS2bim());
+        mEtExame.setText(mMemos.getSExame());
+    }
+
+    @Override
+    protected void onPause(){
+        mMemos.setAll(mEt1bim.getText().toString(), mEt2bim.getText().toString(),
+            mEtExame.getText().toString());
+        mSqlHelper.updateMemos(mMemos);
+
+        super.onPause();
+    }
 }
