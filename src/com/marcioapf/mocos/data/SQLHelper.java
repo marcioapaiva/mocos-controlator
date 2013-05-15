379790f88ec6e3a0ac0bdde327121908a51606bf
Victor Elias <victorgelias@gmail.com>
2013-05-15 01:08:30 -0300

    Optimizing imports for all files.

--- src/com/marcioapf/mocos/data/SQLHelper.java
+++ src/com/marcioapf/mocos/data/SQLHelper.java
@@ -1,13 +1,13 @@
 package com.marcioapf.mocos.data;
 
-import java.util.ArrayList;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
+
+import java.util.ArrayList;
 
 public class SQLHelper extends SQLiteOpenHelper {
 
